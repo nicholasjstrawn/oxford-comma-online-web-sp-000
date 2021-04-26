dfa1511e070cad0560f5fa3a46cb7b7371d97dc0
Nicholas Strawn <nicholasjstrawn@gmail.com>
2021-04-26 21:29:08 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    array.collect do |num|
+      if num == array[-1]
+        array[-1] = "and " << num
+      end
+    end
+    array.join(", ")
+  end
 end
